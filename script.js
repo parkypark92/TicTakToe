e1cfc11ae56959804a59c55dd00ebd555beea59f
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-01-18 18:25:53 +0000

    when a player joins, replace input field with their name. Game board appears once both players have been initialized. Add reset button which returns game to initial state

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+//PLAYERS MODULE
 const players = (function () {
+  let playerOne;
+  let playerTwo;
+  let playersAdded = 0;
+  const newPlayer = function (name) {
+    return { name };
+  };
+
+  let playerOneTurn = true;
+  const inputDisplay1 = document.querySelector(".first-player");
+  const inputDisplay2 = document.querySelector(".second-player");
+  const playerOneName = document.querySelector(".player1-name");
+  playerOneName.classList.add("current-turn");
+  playerOneName.classList.add("hidden");
+  const playerTwoName = document.querySelector(".player2-name");
+  playerTwoName.classList.add("hidden");
   const playerOneInput = document.getElementById("player1");
-  playerOneInput.classList.add("current-turn");
   const playerTwoInput = document.getElementById("player2");
   const addPlayerOne = document.querySelector(".add-player-one");
   const addPlayerTwo = document.querySelector(".add-player-two");
@@ -9,24 +24,58 @@ const players = (function () {
 
   function createFirstPlayer() {
     playerOne = newPlayer(playerOneInput.value);
+    playersAdded++;
+    displayPlayerName(playerOneName, playerOneInput, inputDisplay1);
+    gameBoard.displayBoard(playersAdded);
+    console.log(playerOne);
   }
 
   addPlayerTwo.addEventListener("click", createSecondPlayer);
 
   function createSecondPlayer() {
     playerTwo = newPlayer(playerTwoInput.value);
+    playersAdded++;
+    displayPlayerName(playerTwoName, playerTwoInput, inputDisplay2);
+    gameBoard.displayBoard(playersAdded);
+    console.log(playerTwo);
   }
 
-  function togglePlayerTurn() {
-    playerOneInput.classList.toggle("current-turn");
-    playerTwoInput.classList.toggle("current-turn");
-    playerOneTurn = !playerOneTurn;
+  function displayPlayerName(playerName, input, inputDisplay) {
+    playerName.textContent = input.value;
+    playerName.classList.toggle("hidden");
+    inputDisplay.classList.toggle("hidden");
   }
 
-  return { nextTurn: togglePlayerTurn };
+  function nextTurn() {
+    playerOneName.classList.toggle("current-turn");
+    playerTwoName.classList.toggle("current-turn");
+    players.playerOneTurn = !players.playerOneTurn;
+  }
+
+  function resetPlayers() {
+    playerOne = {};
+    playerTwo = {};
+    playerOneName.classList.toggle("hidden");
+    playerTwoName.classList.toggle("hidden");
+    inputDisplay1.classList.toggle("hidden");
+    inputDisplay2.classList.toggle("hidden");
+    playersAdded = 0;
+  }
+
+  return {
+    playerOne,
+    playerTwo,
+    playerOneTurn,
+    playersAdded,
+    nextTurn,
+    resetPlayers,
+  };
 })();
 
+//GAME BOARD MODULE
 const gameBoard = (function () {
+  const gameplayArea = document.querySelector(".game-play-area");
+  gameplayArea.classList.add("hidden");
   const tl = document.getElementById("top-left");
   const tc = document.getElementById("top-center");
   const tr = document.getElementById("top-right");
@@ -47,20 +96,37 @@ const gameBoard = (function () {
     if (e.target.textContent !== "") {
       return;
     }
-    if (playerOneTurn) {
+    if (players.playerOneTurn) {
       e.target.textContent = "X";
     } else {
       e.target.textContent = "O";
     }
     players.nextTurn();
   }
+
+  function displayBoard(numberOfPlayers) {
+    if (numberOfPlayers === 2) {
+      gameplayArea.classList.toggle("hidden");
+    }
+  }
+
+  function clear() {
+    for (let square of gameSquares) {
+      square.textContent = "";
+    }
+    gameplayArea.classList.toggle("hidden");
+  }
+
+  return { displayBoard, clear };
 })();
 
-let playerOneTurn = true;
+//CONTROLS MODULE
+const gameControls = (function () {
+  const resetButton = document.querySelector(".reset-button");
+  resetButton.addEventListener("click", resetGame);
 
-const newPlayer = function (name) {
-  return { name };
-};
-
-let playerOne;
-let playerTwo;
+  function resetGame() {
+    players.resetPlayers();
+    gameBoard.clear();
+  }
+})();
